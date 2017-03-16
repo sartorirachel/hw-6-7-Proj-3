d4a67c8fabe2a38d8717a335f2144b1400c47f54
Rachel Sartori <sartorir@umich.edu>
2017-03-16 13:02:55 -0400

    completed problem 5

--- SI206W17_HW6.py
+++ SI206W17_HW6.py
@@ -105,7 +105,7 @@ student_tups_list = list(student_tups)
 print("\n\n***** Problem 5 *****")
 # Use a list comprehension to create a list of Student instances out of the student_tups list you just created in Problem 2, and save that list in a variable called programmers. You should make sure you pass these tests before continuing, as you'll need this list for problems later on!
 
-
+programmers = [Student(x, y, z) for x, y, z in student_tups_list]
 
 ## [PROBLEM 6]
 print("\n\n***** Problem 6 *****")
